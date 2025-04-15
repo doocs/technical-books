bbf746d56007ddcadae19e18e8995b6f1330e9a6
Libin YANG <szuyanglb@outlook.com>
2025-04-15 00:19:47 +0000

    feat: add extend page

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,29 +2,31 @@ import DefaultTheme from 'vitepress/theme';
 import giscusTalk from 'vitepress-plugin-comment-with-giscus';
 import { useData, useRoute } from 'vitepress';
 import { toRefs } from "vue";
+import Layout from "./Layout.vue";
 
 export default {
-...DefaultTheme,
-enhanceApp(ctx) {
-DefaultTheme.enhanceApp(ctx);
-},
-setup() {
-const { frontmatter } = toRefs(useData());
-const route = useRoute();
+  extends: DefaultTheme,
+  Layout: Layout,
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx);
+  },
+  setup() {
+    const { frontmatter } = toRefs(useData());
+    const route = useRoute();
 
-        giscusTalk({
-            repo: 'doocs/technical-books',
-            repoId: 'MDEwOlJlcG9zaXRvcnkxNzgwMjQ5NDk=',
-            mapping: 'number',
-            inputPosition: 'top',
-            lang: 'zh-CN',
-            homePageShowComment: true,
-            term: '30',
-            lightTheme: 'light',
-            darkTheme: 'transparent_dark',
-        }, {
-            frontmatter,
-            route
-        }, true);
-    }
+    giscusTalk({
+      repo: 'doocs/technical-books',
+      repoId: 'MDEwOlJlcG9zaXRvcnkxNzgwMjQ5NDk=',
+      mapping: 'number',
+      inputPosition: 'top',
+      lang: 'zh-CN',
+      homePageShowComment: true,
+      term: '30',
+      lightTheme: 'light',
+      darkTheme: 'transparent_dark',
+    }, {
+      frontmatter,
+      route
+    }, true);
+  }
 };
